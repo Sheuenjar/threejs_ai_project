6c06d5d06beee009a600bd21a8d1514b8ffb4e3d
sheuen <sheuenjaramillo@gmail.com>
2023-04-08 17:04:42 -0300

    update for deployment

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,7 +5,7 @@ import { proxy } from 'valtio';
 
 const state = proxy({
   intro: true,
-  color: '#EFBD48',
+  color: '#912a36',
   isLogoTexture: true,
   isFullTexture: false,
   logoDecal: './threejs.png',
